8f5ec893545639468cd7a7ad849d3366d1161602
Allan <amaxwellblair@gmail.com>
2016-04-28 09:49:37 -0600

    Complete second max BST

--- 10_second_max_bst.rb
+++ 10_second_max_bst.rb
@@ -1,3 +1,6 @@
+# 908 938
+# 942 - complete basically correct :D
+
 # Write a function to find the 2nd largest element in a binary search tree ↴ .
 # Here's a sample binary tree node class:
 
@@ -22,7 +25,39 @@
         return @right
     end
 
-    def second_max()
-
+    def max_or_sec_value(node = self)
+      if node.right.nil? && node.left.nil?
+        return node.value
+      elsif node.right.nil?
+        return node.left
+      else
+        max_or_sec_value(node.right)
+      end
     end
+
+    def second_max(node = self)
+      thing = max_or_sec_value()
+      if thing.class == BinaryTreeNode
+        right_most(thing)
+      else
+        return thing
+      end
+    end
+
+    def right_most(node = self)
+      if node.right.nil?
+        return node.value
+      else
+        right_most(node.right)
+      end
+    end
+
 end
+
+tree = BinaryTreeNode.new(8)
+right = tree.insert_right(9).insert_right(12)
+right.insert_left(10)
+tree.insert_left(6).insert_right(7)
+
+p tree.max_or_sec_value()
+p tree.second_max()
